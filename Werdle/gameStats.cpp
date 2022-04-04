c63cf8ea42e52e42a97ac2ad6b397ab5fec3b1eb
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-04-04 16:51:19 +0930

    string format working

--- Werdle/gameStats.cpp
+++ Werdle/gameStats.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 string gameStats::getGameStats()
 {
-	return "\n Played: " + to_string(played) + " Win%: " + to_string(winPercent) + "  Current streak: "+ to_string(currentStreak)+ " Max streak: " + to_string(maxStreak) +"\n \nGUESS DISTRIBUTION\n"+"1: " + to_string(guessDistribution["1"]) + "2: " + to_string(guessDistribution["2"]) + "3: " + to_string(guessDistribution["3"]) + "4: " + to_string(guessDistribution["4"]) + "5: " + to_string(guessDistribution["5"]) + "6: " + to_string(guessDistribution["6"]);
+	return "\nPlayed: " + to_string(played) + " Win%: " + to_string(winPercent) + "  Current streak: "+ to_string(currentStreak)+ " Max streak: " + to_string(maxStreak) +"\n \nGUESS DISTRIBUTION\n"+"\n1: " + to_string(guessDistribution["1"]) + "\n2: " + to_string(guessDistribution["2"]) + "\n3: " + to_string(guessDistribution["3"]) + "\n4: " + to_string(guessDistribution["4"]) + "\n5: " + to_string(guessDistribution["5"]) + "\n6: " + to_string(guessDistribution["6"]);
 }
 
 void gameStats::setPlayed(int game)
